41033bcad37e7045f15b28fed6390d14de11b419
Javier Juan <jjb@wpsnetwork.com>
2018-02-01 16:02:47 +0100

    inicial

---
+++ pre-torneopa/src/main/java/bd/SelectBd.java
@@ -0,0 +1,57 @@
+package bd;
+
+
+
+import java.sql.Connection;
+
+
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * @author www.imagineanddo.com
+ *
+ */
+public class SelectBd {
+
+ /**
+  * @param args
+  */
+ public static void main(String[] args) {
+     String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+     String dbName = "iadDemoDerby";
+     String dbParam = "create=true"; //Si la base de datos no existe, se creará una nueva
+     String dbDirectory = "\\Derby\\";
+     String connectionURL = "jdbc:derby:"+dbDirectory + dbName + ";" + dbParam;
+     
+     Connection conn = null;
+     try{
+       Class.forName(driver);
+     } catch(java.lang.ClassNotFoundException e) {
+       e.printStackTrace();
+     }
+     try {
+       conn = DriverManager.getConnection(connectionURL);
+       Statement statement = conn.createStatement();
+       ResultSet rs = statement.executeQuery("select * from users");
+       
+       while (rs.next()) 
+       { 
+           System.out.println (rs.getString (1) + " " + rs.getString (2)+ " " + rs.getInt(3)); 
+       }
+       
+       
+       
+     }  catch (Throwable e)  {
+       System.out.println("Error en la bd '" + dbName + "'");
+       e.printStackTrace();
+     } finally {
+       try { conn.close(); }
+       catch (Throwable t){}
+     }
+
+ }
+
+}
---
+++ pre-torneopa/src/main/java/bd/crearBD.java
@@ -0,0 +1,65 @@
+package bd;
+
+import java.sql.Connection;
+
+
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * @author www.imagineanddo.com
+ *
+ */
+public class crearBD {
+
+ /**
+  * @param args
+  */
+ public static void main(String[] args) {
+     String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+     String dbName = "iadDemoDerby";
+     String dbParam = "create=true"; //Si la base de datos no existe, se creará una nueva
+     String dbDirectory = "\\Derby\\";
+     String connectionURL = "jdbc:derby:"+dbDirectory + dbName + ";" + dbParam;
+     /*
+     Si no se especifica la ruta donde se creará la base de datos,
+     por defecto se creará en la misma carpeta donde se encuentra el derby.jar
+     En ese caso la cadena de conexió sería la siguiente:
+     String connectionURL = "jdbc:derby:" + dbName + ";" + dbParam;
+     */
+     Connection conn = null;
+     try{
+       Class.forName(driver);
+     } catch(java.lang.ClassNotFoundException e) {
+       e.printStackTrace();
+     }
+     try {
+       conn = DriverManager.getConnection(connectionURL);
+       Statement st = conn.createStatement();
+       String sqlCreateTableUsers =
+              "CREATE TABLE users ( " +
+              "FirstName VARCHAR(20) NOT NULL, " +
+              "LastName VARCHAR(20) NOT NULL, " +
+              "idUser INTEGER NOT NULL CONSTRAINT idUser_PK PRIMARY KEY " +
+              ")";
+       st.execute(sqlCreateTableUsers);
+       System.out.println("La base de datos '" + dbName + "' se ha creado correctamente");
+       
+       
+    st.executeUpdate("INSERT INTO users VALUES('Juan', 'Perez', 1)");
+    st.executeUpdate("INSERT INTO users VALUES('Renzo', 'Lopez', 2)");
+    st.executeUpdate("INSERT INTO users VALUES('Carla', 'Mendivil', 3)");       
+       
+       
+       
+     }  catch (Throwable e)  {
+       System.out.println("Error al crear la base de datos '" + dbName + "'");
+       e.printStackTrace();
+     } finally {
+       try { conn.close(); }
+       catch (Throwable t){}
+     }
+
+ }
+
+}
