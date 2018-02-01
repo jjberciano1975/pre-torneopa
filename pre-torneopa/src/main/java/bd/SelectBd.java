708384b36e691eca7c718f697ea96b15b9b8a69a
Javier Juan <jjb@wpsnetwork.com>
2018-02-01 17:44:38 +0100

    mes der

--- pre-torneopa/src/main/java/bd/SelectBd.java
+++ pre-torneopa/src/main/java/bd/SelectBd.java
@@ -21,10 +21,10 @@ public class SelectBd {
   */
  public static void main(String[] args) {
      String driver = "org.apache.derby.jdbc.EmbeddedDriver";
-     String dbName = "iadDemoDerby";
+     String dbName = "BdPaises";
      String dbParam = "create=true"; //Si la base de datos no existe, se creará una nueva
-     String dbDirectory = "\\Derby\\";
-     String connectionURL = "jdbc:derby:"+dbDirectory + dbName + ";" + dbParam;
+  //   String dbDirectory = "\\Derby\\";
+     String connectionURL = "jdbc:derby:"+ dbName + ";" + dbParam;
      
      Connection conn = null;
      try{
